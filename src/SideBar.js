cfb92dbcccffb853876ecb59c48bc47cb3a389db
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-11-06 17:51:29 -0600

    adding custom color types to tailwind theme for tailwind to create my own set of unique utility classes

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,7 +1,11 @@
 export const SideBar = () => {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shaddow-lg">
-
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+      }
+    },
   },
   variants: {
     extend: {},
