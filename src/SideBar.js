6ba40b7958220fbaed375e2d672aa0468dfdc257
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-11-06 18:36:02 -0600

    creating a custom tailwind class using the layer directive to extend core components. use the apply directive and combine a bunch of utilities in the index.css file

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,7 +1,7 @@
 import { BsPlus, BsFillLightningFill, BsGearFill} from 'react-icons/bs'
 import { FaFire, FaPoo } from 'react-icons/fa'
 
-const SideBar = () => {
+export const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
             <SideBarIcon icon={<FaFire size="28" />} />
@@ -13,9 +13,9 @@ const SideBar = () => {
 }
 
 const SideBarIcon = ({ icon }) => {
-    <div className="sidebar-icon">
-        {icon}
-    </div>
+    return(
+        <div className="sidebar-icon">
+            {icon}
+        </div>
+    )
 }
-
-export default SideBar
